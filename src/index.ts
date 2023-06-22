ac94bfef1ed8cb921b4590f4f78320633d458acb
Durmuş <demirtasdurmus@gmail.com>
2023-06-22 08:18:48 +0300

    test(project): test ci 0

--- src/index.ts
+++ src/index.ts
@@ -4,3 +4,8 @@ server.listen(3000, () => {
     // eslint-disable-next-line no-console
     console.log('Server is listening on port 3000');
 });
+
+const port = process.env.PORT;
+
+// eslint-disable-next-line no-console
+console.log(port);
