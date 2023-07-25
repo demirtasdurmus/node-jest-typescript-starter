5060dd99e91cb55fd876cc3aef36d47b6943090c
Durmuş <durmus.demirtas@anywai.ch>
2023-07-25 17:34:29 +0300

    feat(root): deleted the comment

--- src/index.ts
+++ src/index.ts
@@ -6,5 +6,3 @@ server.listen(port, () => {
     // eslint-disable-next-line no-console
     console.log(`Server is listening on port ${port}`);
 });
-
-// console.log(`Server is listening on port ${port}`).;
