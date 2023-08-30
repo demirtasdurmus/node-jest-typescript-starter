5653c25f670c7275f78ddc081fe4a258a3253cd7
Durmuş <durmus.demirtas@anywai.ch>
2023-08-30 18:52:39 +0300

    ci: try different danger bot-final

--- src/index.ts
+++ src/index.ts
@@ -4,5 +4,5 @@ const port = process.env.PORT || 3000;
 
 server.listen(port, () => {
     // eslint-disable-next-line no-console
-    console.log(`Server is listening on port ${port}.`);
+    console.log(`Server is listening on port ${port}`);
 });
