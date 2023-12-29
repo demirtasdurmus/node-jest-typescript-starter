df76459fdbd3a0ffcc191ab9fb1d21596c4ea4b1
Durmuş <durmus.demirtas@anywai.ch>
2023-12-29 18:30:27 +0300

    feat: add arbitrary comment for new staging release

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,10 @@ import { server } from './server';
 
 const port = process.env.PORT || 3000;
 
+/**
+ * This comment will be regarded as a new feature for the staging environment
+ */
+
 server.listen(port, () => {
     // eslint-disable-next-line no-console
     console.log(`Server is listening on port ${port}`);
